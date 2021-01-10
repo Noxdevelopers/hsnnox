7373011df19fd002cb0ed832179fc707f4bd8d86
Amir Reza <amirrezaghahremani@gmail.com>
2021-01-10 15:31:35 +0330

    route

--- ROUTE.php
+++ ROUTE.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "Hello Word this is my life!";
+define("ROUTE"           , "ROUTE");
+define("ACTION"          , "ACTION");
+//Users
+define("ROUTE_USERS"     , "USERS");
+define("ACTION_LOGIN"    , "LOGIN");
+define("ACTION_REGISTER" , "REGISTER");
+//Products
+define("ROUTE_PRODUCTS"  , "PRODUCTS");
+define("ACTION_ADD"      , "ADD");
+define("ACTION_READ"     , "READ");
+define("ACTION_EDIT"     , "EDIT");
+define("ACTION_DELETE"   , "DELETE");
+define("INPUT_IMAGE"     , "IMAGE");
